675291f63fe47a086b49a65d3b29f43bbe7dd52b
BrandonRuf <91632371+BrandonRuf@users.noreply.github.com>
2024-07-03 11:38:44 -0400

    Split the serial_gui_base into separate file for readability

--- temperature_controller.py
+++ temperature_controller.py
@@ -1,14 +1,12 @@
 import spinmob.egg   as _egg
 import spinmob       as _s
-import mcphysics     as _mc
 import time          as _time
-import os            as _os
 import serial        as _serial
 
 _g            = _egg.gui
-_serial_tools = _mc.instruments._serial_tools
 
 from serial.tools.list_ports import comports as _comports
+from _serial_gui_base import serial_gui_base
 from _arduino_api import arduino_api
 
 # GUI settings
@@ -17,279 +15,6 @@ _s.settings['dark_theme_qt'] = True
 # Print data to terminal for debugging
 _debug = True
 
-
-class serial_gui_base(_g.BaseObject):
-    """
-    Base class for creating a serial connection gui. Handles common controls.
-    
-    Parameters
-    ----------
-    api_class=None : class
-        Class to use when connecting. For example, api_class=auber_syl53x2p_api would
-        work. Note this is not an instance, but the class itself. An instance is
-        created when you connect and stored in self.api.
-        
-    name='serial_gui' : str
-        Unique name to give this instance, so that its settings will not
-        collide with other egg objects.
-        
-    show=True : bool
-        Whether to show the window after creating.
-        
-    block=False : bool
-        Whether to block the console when showing the window.
-        
-    window_size=[1,1] : list
-        Dimensions of the window.
-    hide_address=False: bool
-        Whether to show the address control for things like the Auber.
-    """
-    def __init__(self, api_class=None, name='serial_gui', show=True, block=False, window_size=[1,1], hide_address=False):
-
-        # Remebmer the name.
-        self.name = name
-
-        # Checks periodically for the last exception
-        self.timer_exceptions = _g.TimerExceptions()
-        self.timer_exceptions.signal_new_exception.connect(self._new_exception)
-
-        # Where the actual api will live after we connect.
-        self.api = None
-        self._api_class = api_class
-
-        # GUI stuff
-        self.window   = _g.Window(
-            self.name, size=window_size, autosettings_path=name+'.window',
-            event_close = self._window_close)
-        
-        # Top of GUI (Serial Communications)
-        self.grid_top = self.window.place_object(_g.GridLayout(margins=False), alignment=0)
-        self.window.new_autorow()
-        self.grid_bot = self.window.place_object(_g.GridLayout(margins=False), alignment=0)
-
-        # Get all the available ports
-        self._label_port = self.grid_top.add(_g.Label('Port:'))
-        self._ports = [] # Actual port names for connecting
-        ports       = [] # Pretty port names for combo box
-        if _comports:
-            for p in _comports():
-                self._ports.append(p.device)
-                ports      .append(p.description)
-        
-        # Append simulation port
-        ports      .append('Simulation')
-        self._ports.append('Simulation')
-        
-        # Append refresh port
-        ports      .append('Refresh - Update Ports List')
-        self._ports.append('Refresh - Update Ports List')
-        
-        self.combo_ports = self.grid_top.add(_g.ComboBox(ports, autosettings_path=name+'.combo_ports'))
-        self.combo_ports.signal_changed.connect(self._ports_changed)
-
-        self.grid_top.add(_g.Label('Address:')).show(hide_address)
-        self.number_address = self.grid_top.add(_g.NumberBox(
-            1, 1, int=True,
-            autosettings_path=name+'.number_address',
-            tip='Address (not used for every instrument)')).set_width(40).show(hide_address)
-
-        self.grid_top.add(_g.Label('Baud:'))
-        self.combo_baudrates = self.grid_top.add(_g.ComboBox(
-            ['1200', '2400', '4800', '9600', '19200', '115200'],
-            default_index=5,
-            autosettings_path=name+'.combo_baudrates'))
-
-        self.grid_top.add(_g.Label('Timeout:'))
-        self.number_timeout = self.grid_top.add(_g.NumberBox(50, dec=True, bounds=(1, None), suffix=' ms', tip='How long to wait for an answer before giving up (ms).', autosettings_path=name+'.number_timeout')).set_width(100)
-
-        # Button to connect
-        self.button_connect  = self.grid_top.add(_g.Button('Connect', checkable=True))
-
-        # Stretch remaining space
-        self.grid_top.set_column_stretch(self.grid_top._auto_column)
-
-        # Connect signals
-        self.button_connect.signal_toggled.connect(self._button_connect_toggled)
-        
-        # Status
-        self.label_status = self.grid_top.add(_g.Label(''))
-
-        # Expand the bottom grid
-        self.window.set_row_stretch(1)
-        
-        # Error
-        self.grid_top.new_autorow()
-        self.label_message = self.grid_top.add(_g.Label(''), column_span=10).set_colors('pink' if _s.settings['dark_theme_qt'] else 'red')
-        
-        # By default the bottom grid is disabled
-        self.grid_bot.disable()
-
-        # Other data
-        self.t0 = None
-
-        # Run the base object stuff and autoload settings
-        _g.BaseObject.__init__(self, autosettings_path=name)
-
-        # Show the window.
-        if show: self.window.show(block)
-    
-    def _ports_changed(self):
-        """
-        Refreshes the list of availible serial ports in the GUI.
-        """
-        
-        if self.get_selected_port() == 'Refresh - Update Ports List':
-            
-            len_ports = len(self.combo_ports.get_all_items())
-            
-            # Clear existing ports
-            if(len_ports > 1): # Stop recursion!
-                for n in range(len_ports):
-                    self.combo_ports.remove_item(0)
-            else:
-                return
-                self.combo_ports.remove_item(0)
-                 
-            self._ports = [] # Actual port names for connecting
-            ports       = [] # Pretty port names for combo box
-                
-            default_port = 0
-             
-            # Get all the available ports
-            if _comports:
-                for inx, p in enumerate(_comports()):
-                    self._ports.append(p.device)
-                    ports      .append(p.description)
-                    
-                    if 'Arduino' in p.description:
-                        default_port = inx
-                        
-            # Append simulation port
-            ports      .append('Simulation')
-            self._ports.append('Simulation')
-            
-            # Append refresh port
-            ports      .append('Refresh - Update Ports List')
-            self._ports.append('Refresh - Update Ports List')
-             
-            # Add the new list of ports
-            for item in ports:
-                self.combo_ports.add_item(item)
-             
-            # Set the new default port
-            self.combo_ports.set_index(default_port)
-    
-    def _button_connect_toggled(self, *a):
-        """
-        Connect by creating the API.
-        """
-        if self._api_class is None:
-            raise Exception('You need to specify an api_class when creating a serial GUI object.')
-
-        # If we checked it, open the connection and start the timer.
-        if self.button_connect.is_checked():
-            port = self.get_selected_port()
-            self.api = self._api_class(
-                    port=port,
-                    address=self.number_address.get_value(),
-                    baudrate=int(self.combo_baudrates.get_text()),
-                    timeout=self.number_timeout.get_value())
-            
-            # Delay to give the Arduino time to run setup
-            _time.sleep(2)
-
-            # Record the time if it's not already there.
-            if self.t0 is None: self.t0 = _time.time()
-
-            # Enable the grid
-            self.grid_bot.enable()
-
-            # Disable serial controls
-            self.combo_baudrates.disable()
-            self.combo_ports    .disable()
-            self.number_timeout .disable()
-            
-            if self.api.simulation_mode:
-                #self.label_status.set_text('*** Simulation Mode ***')
-                #self.label_status.set_colors('pink' if _s.settings['dark_theme_qt'] else 'red')
-                self.combo_ports.set_value(len(self._ports)-2)
-                self.button_connect.set_text("Simulation").set_colors(background='pink')
-            else:
-                self.button_connect.set_text('Disconnect').set_colors(background = 'blue')
-
-        # Otherwise, shut it down
-        else:
-            self.api.disconnect()
-            #self.label_status.set_text('')
-            self.button_connect.set_colors()
-            self.grid_bot.disable()
-
-            # Enable serial controls
-            self.combo_baudrates.enable()
-            self.combo_ports    .enable()
-            self.number_timeout .enable()
-            
-            self.button_connect.set_text('Connect').set_colors(background = '')
-
-
-        # User function
-        self._after_button_connect_toggled()
-
-    def _after_button_connect_toggled(self):
-        """
-        Dummy function called after connecting.
-        """
-        return
-
-    def _new_exception(self, a):
-        """
-        Just updates the status with the exception.
-        """
-        self.label_message(str(a)).set_colors('red')
-
-    def _window_close(self):
-        """
-        Disconnects. When you close the window.
-        """
-        print('Window closed but not destroyed. Use show() to bring it back.')
-        if self.button_connect():
-            print('  Disconnecting...')
-            self.button_connect(False)
-
-    def get_selected_port(self):
-        """
-        Returns the actual port string from the combo box.
-        """
-        return self._ports[self.combo_ports.get_index()]
-    
-    def get_com_ports():
-        """
-        Returns a dictionary of port names as keys and descriptive names as values.
-        """
-        if _comports:
-    
-            ports = dict()
-            for p in _comports(): ports[p.device] = p.description
-            return ports
-    
-        else:
-            raise Exception('You need to install pyserial and have Windows to use get_com_ports().')
-            
-    def list_com_ports():
-        """
-        Prints a "nice" list of available COM ports.
-        """
-        ports = get_com_ports()
-    
-        # Empty dictionary is skipped.
-        if ports:
-            keys = list(ports.keys())
-            keys.sort()
-            print('Available Ports:')
-            for key in keys:
-                print(' ', key, ':', ports[key])
-    
-        else: raise Exception('No ports available. :(')
 
 class temperature_controller(serial_gui_base):
     """
@@ -484,11 +209,11 @@ class temperature_controller(serial_gui_base):
             autosettings_path=name+'.plot',
             delimiter=',', show_logger=True), alignment=0, column_span=10)
         
-        # Channel 0 tab segmentation
+        # Channel 1 tab segmentation
         t11 = self.tab_channel_1.place_object(_g.GridLayout(margins=False), alignment=0, row=1,column=0)
         t12 = self.tab_channel_1.place_object(_g.GridLayout(margins=False), alignment=0, row=1,column=1)
         t13 = self.tab_channel_1.place_object(_g.GridLayout(margins=False), alignment=0, row=1,column=2)
-        t14 = self.tab_channel_1.place_object(_g.GridLayout(margins=False), alignment=0, row=3, column = 0,column_span=10)
+        t14 = self.tab_channel_1.place_object(_g.GridLayout(margins=False), alignment=0, row=2, column = 0,column_span=10)
         
         t11.add(_g.Label('Temperature:'), alignment=2).set_style('font-size: 15pt; font-weight: bold; color: white')
         self.number_temperature_1 = t11.add(_g.NumberBox(
@@ -525,7 +250,7 @@ class temperature_controller(serial_gui_base):
         t13.add(_g.Label('Channel 1'), alignment=0, column=0, row=0).set_style('font-size: 20pt; color: fuchsia')
         self.channel_1_status = t13.add(_g.Label('(Disconnected)')   , alignment=0, column=0, row=1).set_style('font-size: 20pt; color: coral')
 
-        # Make the Channel 0 plotter.
+        # Make the Channel 1 plotter.
         self.plot_1 = t14.add(_g.DataboxPlot(
             file_type='*.csv',
             autosettings_path=name+'.plot',
